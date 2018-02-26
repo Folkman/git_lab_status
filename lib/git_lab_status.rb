50d4f36174f54917922f503d979c19e541268733
Lee Folkman <narrowwaymedia@gmail.com>
2018-02-26 16:45:14 -0600

    Add method for https://about.gitlab.com with tests

--- lib/git_lab_status.rb
+++ lib/git_lab_status.rb
@@ -3,27 +3,54 @@ require 'open-uri'
 
 module GitLabStatus
 
+  @interval   = 10
+  @total_time = 60
 
-  # Returns average response time rounded to two decimals
-  # Accepts interval and total_time for purpose of testing
-  def self.home_page_resp(interval=10, total_time=60)
+
+  def self.home_page_resp
+    page_resp('https://gitlab.com')
+  end
+
+
+  def self.about_page_resp
+    page_resp('https://about.gitlab.com')
+  end
+
+
+private
+
+
+  def self.interval
+    @interval
+  end
+
+
+  def self.total_time
+    @total_time
+  end
+
+
+  def self.page_resp(url)
     first_check = true
     resp_times = []
     start_minute = Time.now
     start_load_time = start_minute
 
     loop do
-      if first_check || Time.now - start_load_time >= interval
+      if first_check || (Time.now - start_load_time) >= interval
         start_load_time = Time.now
-        open('https://gitlab.com')
+        open(url)
         load_time = Time.now - start_load_time
         resp_times << load_time
+        print '.'
       end
 
       first_check = false
 
       break if Time.now - start_minute >= total_time
     end
+
+    puts
 
     resp_times.reduce(:+).fdiv(resp_times.size).round(2)
   end
--- spec/git_lab_status_spec.rb
+++ spec/git_lab_status_spec.rb
@@ -5,9 +5,23 @@ RSpec.describe GitLabStatus do
   end
 
 
-  it "gives the average response time every ten seconds for one minute for https://gitlab.com" do
-    allow(GitLabStatus).to receive(:open) { sleep 0.05 } # Simulate response time
-    expect(GitLabStatus.home_page_resp(0.1, 1)).to eq(0.05)
+  context "methods" do
+    before(:each) do
+      allow(GitLabStatus).to receive(:interval).and_return(0.1)
+      allow(GitLabStatus).to receive(:total_time).and_return(0.3)
+    end
+
+
+    it "gives the average response time every ten seconds for one minute for https://gitlab.com" do
+      allow(GitLabStatus).to receive(:open) { sleep 0.05 } # Simulate response time
+      expect(GitLabStatus.home_page_resp).to eq(0.05)
+    end
+
+
+    it "gives the average response time every ten seconds for one minute for https://about.gitlab.com" do
+      allow(GitLabStatus).to receive(:open) { sleep 0.07 } # Simulate response time
+      expect(GitLabStatus.about_page_resp).to eq(0.07)
+    end
   end
 
 
