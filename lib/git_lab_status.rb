69a4bf97b3b2f0701f31d41589f1c6f63312e8ae
Lee Folkman <narrowwaymedia@gmail.com>
2018-02-26 11:24:56 -0600

    Add method with tests for getting home page

--- lib/git_lab_status.rb
+++ lib/git_lab_status.rb
@@ -1,5 +1,32 @@
-require "git_lab_status/version"
+require 'git_lab_status/version'
+require 'open-uri'
 
 module GitLabStatus
-  # Your code goes here...
+
+
+  # Returns average response time rounded to two decimals
+  # Accepts interval and total_time for purpose of testing
+  def self.home_page_resp(interval=10, total_time=60)
+    first_check = true
+    resp_times = []
+    start_minute = Time.now
+    start_load_time = start_minute
+
+    loop do
+      if first_check || Time.now - start_load_time >= interval
+        start_load_time = Time.now
+        open('https://gitlab.com')
+        load_time = Time.now - start_load_time
+        resp_times << load_time
+      end
+
+      first_check = false
+
+      break if Time.now - start_minute >= total_time
+    end
+
+    resp_times.reduce(:+).fdiv(resp_times.size).round(2)
+  end
+
+
 end
--- spec/git_lab_status_spec.rb
+++ spec/git_lab_status_spec.rb
@@ -1,9 +1,14 @@
 RSpec.describe GitLabStatus do
+
   it "has a version number" do
     expect(GitLabStatus::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+  it "gives the average response time every ten seconds for one minute for https://gitlab.com" do
+    allow(GitLabStatus).to receive(:open) { sleep 0.05 } # Simulate response time
+    expect(GitLabStatus.home_page_resp(0.1, 1)).to eq(0.05)
   end
+
+
 end
